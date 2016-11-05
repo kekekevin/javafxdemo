0c47f2593230167f6f14031b6154916f98baa090
Kevin Tung <kekekevin@users.noreply.github.com>
2016-11-05 14:32:54 -0500

    validation

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,33 +16,28 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
-        gridPane.setVgap(8);
-        gridPane.setHgap(10);
+        TextField nameInput = new TextField();
 
-        Label nameLabel = new Label("Username:");
-        GridPane.setConstraints(nameLabel, 0, 0);
+        Button button = new Button("Click me");
+        button.setOnAction(e -> isInt(nameInput, nameInput.getText()));
 
-        TextField nameInput = new TextField("Bucky");
-        GridPane.setConstraints(nameInput, 1, 0);
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(nameInput, button);
 
-        Label passwordLabel = new Label("Password:");
-        GridPane.setConstraints(passwordLabel, 0, 1);
-
-        TextField passwordInput = new TextField();
-        passwordInput.setPromptText("password");
-        GridPane.setConstraints(passwordInput, 1, 1);
-
-        Button loginButton = new Button("Log In");
-
-        GridPane.setConstraints(loginButton, 1, 2);
-
-        gridPane.getChildren().addAll(nameLabel, nameInput, passwordLabel, passwordInput, loginButton);
-
-        primaryStage.setScene(new Scene(gridPane));
+        primaryStage.setScene(new Scene(layout));
         primaryStage.setTitle("title goes here");
 
         primaryStage.show();
+    }
+
+    private boolean isInt(TextField input, String message) {
+        try {
+            int value = Integer.parseInt(input.getText());
+            System.out.println("user is " + value);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("error");
+        }
+        return false;
     }
 }
