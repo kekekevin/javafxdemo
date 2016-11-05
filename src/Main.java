3fb484824c2adb033b62260af6ed5721ca8d79a4
Kevin Tung <kekekevin@users.noreply.github.com>
2016-11-05 15:10:23 -0500

    listview yayy

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,21 +23,32 @@ public class Main extends Application {
 
         Button button = new Button("pick me!");
 
-        ComboBox<String> comboBox = new ComboBox<>();
-        comboBox.getItems().addAll("Good Will Hunting", "St. Vincent", "Blackhat");
-        comboBox.setPromptText("What is your favorite movie?");
-        button.setOnAction(e -> printMovie(comboBox));
-        comboBox.setOnAction(e -> System.out.println("user selected: " + comboBox.getValue()));
-        comboBox.setEditable(true);
+        ListView<String> listView = new ListView<>();
+        listView.getItems().addAll("Iron Man", "Titanic", "Contact", "Surrogates");
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        button.setOnAction(e -> buttonClicked(listView));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(10, 10, 10, 10));
-        layout.getChildren().addAll(comboBox, button);
+        layout.getChildren().addAll(listView, button);
 
         primaryStage.setScene(new Scene(layout));
         primaryStage.setTitle("title goes here");
 
         primaryStage.show();
+    }
+
+    private void buttonClicked(ListView<String> listView) {
+        String message = "";
+        ObservableList<String> movies;
+
+        movies = listView.getSelectionModel().getSelectedItems();
+
+        for (String m : movies) {
+            message += m + "\n";
+        }
+        System.out.println(message);
     }
 
     private void printMovie(ComboBox<String> comboBox) {
