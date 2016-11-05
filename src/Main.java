86aed310635b93db90bbe1251907dcac951f8cd2
Kevin Tung <kekekevin@users.noreply.github.com>
2016-11-05 13:48:13 -0500

    alert modal

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,19 +18,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Label label1 = new Label("Welcome to the first scene");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> primaryStage.setScene(scene2));
+        Button button1 = new Button("Yo dawg");
+        button1.setOnAction(e -> AlertBox.display("title of alert box", "alert box is amazing"));
 
         VBox layout1 = new VBox(20);
         layout1.getChildren().addAll(label1, button1);
         scene1 = new Scene(layout1, 200, 200);
-
-        Button button2 = new Button("this scene sucks, go back to scene 1");
-        button2.setOnAction(e -> primaryStage.setScene(scene1));
-
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().addAll(button2);
-        scene2 = new Scene(layout2, 600, 300);
 
         primaryStage.setScene(scene1);
         primaryStage.setTitle("title goes here");
