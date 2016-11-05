f6f4035350111c925b8dea84a261bc78b0090e85
Kevin Tung <kekekevin@users.noreply.github.com>
2016-11-05 15:56:13 -0500

    tableview

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,35 +24,47 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
 
-        Button button = new Button("pick me!");
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        TreeItem<String> root, bucky, megan;
+        TableColumn<Product, String> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(100);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 
-        root = new TreeItem<>();
-        root.setExpanded(true);
+        TableColumn<Product, String> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(100);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
-        bucky = makeBranch("Bucky", root);
-        makeBranch("thenewboston", bucky);
-        makeBranch("YouTube", bucky);
-        makeBranch("Chicken", bucky);
+        TextField nameInput = new TextField();
+        nameInput.setPromptText("Name");
+        nameInput.setMinWidth(100);
 
-        megan = makeBranch("Megan", root);
-        makeBranch("Glitter", megan);
-        makeBranch("Makeup", megan);
+        TextField priceInput = new TextField();
+        priceInput.setPromptText("Price");
+        priceInput.setMinWidth(100);
 
-        TreeView<String> tree = new TreeView<>(root);
-        tree.setShowRoot(false);
-        tree.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> {
-            if (newValue != null) {
-                System.out.println(newValue);
-            }
+        TextField quantityInput = new TextField();
+        quantityInput.setPromptText("Quantity");
+        quantityInput.setMinWidth(100);
 
-        });
+        TableView<Product> table = new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e -> addButtonClicked(table, nameInput, priceInput, quantityInput));
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(e -> deleteButtonClicked(table));
+
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(10, 10, 10, 10));
+        hbox.setSpacing(10);
+        hbox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(10, 10, 10, 10));
-        layout.getChildren().addAll(button);
-        layout.getChildren().add(tree);
+        layout.getChildren().addAll(table, hbox);
 
         primaryStage.setScene(new Scene(layout));
         primaryStage.setTitle("title goes here");
@@ -55,11 +72,34 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
-        TreeItem<String> treeItem = new TreeItem<>(title);
-        treeItem.setExpanded(true);
-        parent.getChildren().add(treeItem);
+    private void deleteButtonClicked(TableView<Product> table) {
+        ObservableList<Product> productsSelected, allProducts;
+        allProducts = table.getItems();
+        productsSelected = table.getSelectionModel().getSelectedItems();
 
-        return treeItem;
+        productsSelected.forEach(allProducts::remove);
+    }
+
+    private void addButtonClicked(TableView<Product> table, TextField nameInput, TextField priceInput, TextField quantityInput) {
+        Product product = new Product();
+        product.setName(nameInput.getText());
+        product.setPrice(Double.parseDouble(priceInput.getText()));
+        product.setQuantity(Integer.parseInt(quantityInput.getText()));
+
+        table.getItems().add(product);
+
+        nameInput.clear();
+        priceInput.clear();
+        quantityInput.clear();
+    }
+
+    public ObservableList<Product> getProduct() {
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product("Laptop", 999.88, 20));
+        products.add(new Product("Bouncy Ball", 2.88, 200));
+        products.add(new Product("Toilet", 99.00, 70));
+        products.add(new Product("Corn", 1.00, 790));
+
+        return products;
     }
 }
